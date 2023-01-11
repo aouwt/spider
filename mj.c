3e2852a611b6f617d345672846954b5e1bc022e5
kit <kit@debianjr.res6.spectrum.com>
2023-01-10 19:53:51 -0500

    .

--- mj.c
+++ mj.c
@@ -15,22 +15,22 @@
 
 
 #define FRAME_TIME (1000 / 60)
-
+#define LEGSZ	32
 
 #define THROWSDLERR	fprintf (stderr, "SDL error at %u: %s\n", (unsigned int) __LINE__, SDL_GetError ())
 
-#define SDLERRNZ(func)	{	\
-	if ((func) != 0) {	\
-		THROWSDLERR;	\
-		exit (1);	\
-	}	\
+#define SDLERRNZ(func)	{\
+	if ((func) != 0) {\
+		THROWSDLERR;\
+		exit (1);\
+	}\
 }
 
-#define SDLERRNULL(func)	{	\
-	if ((func) == NULL) {	\
-		THROWSDLERR;	\
-		exit (1);	\
-	}	\
+#define SDLERRNULL(func)	{\
+	if ((func) == NULL) {\
+		THROWSDLERR;\
+		exit (1);\
+	}\
 }
 
 
@@ -70,26 +70,38 @@ SDL_Texture *BackgroundTexture = NULL;
 SDL_Event Event;
 
 int MouseX, MouseY;
-Point Leg [9] = { 0 };
+SDL_Rect Viewport;
+Coord Leg [9] = { 0 };
 
 
 void GenAttachTable (void) {
 	
 	SDL_PixelFormat *fmt;
-	SDLERRNULL	(fmt = SDL_AllocFormat (SDL_BYTESPERPIXEL (8)));
+	SDLERRNULL	(fmt = SDL_AllocFormat (SDL_PIXELFORMAT_RGB332));
 	
 	SDL_Surface *img;
 	SDLERRNULL	(img = SDL_ConvertSurface (BackgroundSurface, fmt, 0));
 	
-	SDL_Rect v;
-	SDL_RenderGetViewport (Renderer, &v);
-	
-	AttachTo = malloc (sizeof (AttachTo [0]) * v->w * v->h);
+	AttachTo = malloc (sizeof (AttachTo [0]) * (Viewport.w+1) * (Viewport.h+1) + 1);
 	
 	SDLERRNZ	(SDL_LockSurface (img));
 	
-	for (int px = 0; px != v->w; px ++) {
-		for (int py = 0; py != v->h; py ++) {
+	#define pix(x,y)	((x) * Viewport.w + (y))
+	#define check	{\
+		if (ox >= 0 && ox < Viewport.w && oy >= 0 && oy < Viewport.h) {\
+			if (*((uint8_t *) img->pixels + pix (ox, oy)) != 0) {\
+				unsigned int dist = (ox * ox) + (oy * oy);\
+				if (dist < closest) {\
+					closest = dist;\
+					closestat.x = ox;\
+					closestat.y = oy;\
+				}\
+			}\
+		}\
+	}
+	
+	for (int px = 0; px != Viewport.w; px ++) {
+		for (int py = 0; py != Viewport.h; py ++) {
 			for (unsigned short leg = 0; leg != 8; leg ++) {
 				int x, y;
 				if (leg > 4)
@@ -98,10 +110,39 @@ void GenAttachTable (void) {
 					x = px + 64;
 				y = py + ((py % 5) * 16);
 				
+				if (x < 0 || y < 0 || x > Viewport.w || y > Viewport.h)
+					continue;
+				
 				// do things or whatever
+				int sz_max = LEGSZ * 2 * 2;
+				const unsigned int max_dist = pow (LEGSZ, 2);
+				unsigned int closest = -1;
+				Coord closestat;
+				
+				signed short dir = 1;
+				int ox = 0;
+				int oy = 0;
+				for (int sz = 1; sz != sz_max; sz ++) {
+					while (2 * ox * dir < sz) {
+						check;
+						ox += dir;
+					}
+					while (2 * oy * dir < sz) {
+						check;
+						oy += dir;
+					}
+					dir *= -1;
+				}
+				
+				if (closest > max_dist)
+					closestat.x = closestat.y = -1;
+				
+				AttachTo [pix (x, y)].l [leg] = closestat;
 			}
 		}
 	}
+	#undef pix
+	#undef check
 	
 	SDL_UnlockSurface (img);
 	SDL_FreeSurface (img);
@@ -114,7 +155,7 @@ void SolveTriangle_ABC (Triangle *t) {
 	t->C = ACOS ((POW (t->a,2) + POW (t->b,2) - POW (t->c,2)) / (2 * t->a * t->b));
 }
 
-void GetTriangleCoord (const Point *C, const Triangle *t, TriangleCoord *c, signed short flip) {
+void GetTriangleCoord (const Coord *C, const Triangle *t, TriangleCoord *c, signed short flip) {
 	// A = (0, 0)
 	// B = ((Cx cos(alpha) - Cy sin(alpha)) * c/b, (Cy cos(alpha) + Cx sin(alpha)) * c/b)
 	// C = (Cx, Cy)
@@ -129,29 +170,31 @@ void GetTriangleCoord (const Point *C, const Triangle *t, TriangleCoord *c, sign
 	c->B.y = (c->C.y * _cos + c->C.x * _sin) * mult;
 }
 
-void DrawTriangle (const TriangleCoord *c, const Point *a) {
+void DrawTriangle (const TriangleCoord *c, const Coord *a) {
 	SDL_Point p [3];
 	p [0].x = c->A.x + a->x; p [0].y = c->A.y + a->y;
 	p [1].x = c->B.x + a->x; p [1].y = c->B.y + a->y;
 	p [2].x = c->C.x + a->x; p [2].y = c->C.y + a->y;
 	SDLERRNZ	(SDL_RenderDrawLines (Renderer, p, 3));
 }
-bool LegThings (int, int, unsigned short);
-bool ReLeg (int x, int y, unsigned short leg) {
+
+bool ReLeg (int x, int y, short leg) {
 	/*if (leg >= 4)
 		Leg [leg].x = x + 50;
 	else
 		Leg [leg].x = x - 50;
 	Leg [leg].y = y + (leg % 5) * 16;*/
 	
+	Leg [leg] = AttachTo [x * Viewport.w + y].l [leg];
+	
 	return false; //LegThings (x, y, leg);
 }
 
-bool LegThings (int x, int y, unsigned short leg) {
+bool LegThings (int x, int y, short leg) {
 
-	Point A = Leg [leg];
+	Coord A = Leg [leg];
 	
-	Point C;
+	Coord C;
 	C.x = x - A.x;
 	C.y = y - A.y;
 	
@@ -181,6 +224,7 @@ bool LegThings (int x, int y, unsigned short leg) {
 	return false;
 }
 
+
 void DoThings (void) {
 	static float px = 0;
 	static float py = 0;
@@ -193,7 +237,7 @@ void DoThings (void) {
 	SDLERRNZ	(SDL_RenderCopy (Renderer, BackgroundTexture, NULL, NULL));
 	
 	SDLERRNZ	(SDL_SetRenderDrawColor (Renderer, 255, 255, 255, 0));
-	for (unsigned short i = 0; i != 8; i ++)
+	for (short i = 0; i != 8; i ++)
 		if (LegThings (px, py, i))
 			break;
 	SDL_RenderPresent (Renderer);
@@ -220,8 +264,25 @@ int main (void) {
 	SDLERRNULL	(Window);
 	SDLERRNULL	(Renderer);
 	
-	BackgroundSurface = SDL_LoadBMP ("bkg.bmp");
-	SDLERRNULL	(BackgroundSurface);
+	SDL_RenderGetViewport (Renderer, &Viewport);
+	
+	{
+		SDL_Surface *bkg = SDL_LoadBMP ("bkg.bmp");
+		SDLERRNULL	(bkg);
+		
+		Uint32 fmt = SDL_GetWindowPixelFormat (Window);
+		BackgroundSurface = SDL_CreateRGBSurfaceWithFormat (
+			0,
+			Viewport.w, Viewport.h,
+			SDL_BITSPERPIXEL (fmt), fmt
+		);
+		SDLERRNULL	(BackgroundSurface);
+		
+		SDLERRNZ	(SDL_BlitScaled (bkg, NULL, BackgroundSurface, NULL));
+		
+		SDL_FreeSurface (bkg);
+	}
+	GenAttachTable ();
 	
 	BackgroundTexture = SDL_CreateTextureFromSurface (Renderer, BackgroundSurface);
 	SDLERRNULL	(BackgroundTexture);
